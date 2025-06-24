fd8fc9a1be18c6d2e156074cfd77732da3b73051
Rodrigo Soares <adsrodsoares@gmail.com>
2025-06-24 15:46:20 -0300

    Criando serviço de conexão com a Open AI - Parte 02

--- src/services/sentimentService.js
+++ src/services/sentimentService.js
@@ -3,4 +3,17 @@ const axios = require('axios');
 getSentimento = () = async (prompt) => {
     const commandprompt = `Retorne-me o sentimento que a frase a seguir expressa (se bom ou ruim): ${prompt}`;
 
+    const response = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+            model: 'gpt-3.5-turbo',
+            messages: [{role: 'user', content: commandprompt}]
+        },
+        {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPEN_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        }
+    );
 }
