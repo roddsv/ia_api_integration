9f310ea25c47efc019cb02031abdfebe809e3834
Rodrigo Soares <adsrodsoares@gmail.com>
2025-06-24 14:51:33 -0300

    Usando router do express, criando e consumindo rota de teste

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, ()=> console.log("API rodando na porta 3000"));
