02547157b1f9015d38be2e949af0e79bf7ef7def
Rodrigo Soares <adsrodsoares@gmail.com>
2025-06-24 15:11:17 -0300

    Criando serviço de conexão com a Open AI - Parte 01

---
+++ src/services/sentimentService.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+
+getSentimento = () = async (prompt) => {
+    const commandprompt = `Retorne-me o sentimento que a frase a seguir expressa (se bom ou ruim): ${prompt}`;
+
+}
