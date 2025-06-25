22e763de4e62575680bce62de0cfd3b0bdbb1647
Rodrigo Soares <adsrodsoares@gmail.com>
2025-06-24 23:22:42 -0300

    Implementando lógica de análise no controller

--- src/routes/controllers/analyzeSentiment.js
+++ src/routes/controllers/analyzeSentiment.js
@@ -1,15 +1,20 @@
 const sentimentService = require('../../services/sentimentService');
+const wordAnalysisService = require('../../services/wordAnalysisService');
 
 analyzeSentimentText = async (req, res, next) => {
     try {
         const { text } = req.body;
         if (!text) {
             return res.status(400).json({ error: 'Nenhuma frase inserida para análise de sentimento'});
-
-            const sentiment = await sentimentService.getSentimento(text);
-
-            res.status(200).json({'sentimento': sentiment});
         }
+
+        const wordAmountData = wordAnalysisService.getPromptAmount(text);
+        const sentiment = await sentimentService.getSentimento(text);
+
+        finalAnalysis = { ...wordAmountData, sentiment, raw: text };
+
+        res.status(200).json({'sentimento': sentiment});
+        
     } catch (e) {
         next(e);
     }
--- src/services/wordAnalysisService.js
+++ src/services/wordAnalysisService.js
@@ -1,5 +1,22 @@
 const stopWords = require('../utils/stopwords');
 
 getPromptAmount = (text) => {
-    const words = text.toLowerCase().match()
+    const words = text.toLowerCase().match(/\b\w+\b/g)
+    .filter(word => !stopwords.includes(word));
+
+    const wordCounter = words.length;
+
+    const frequence = {};
+
+    words.forEach(w =>
+        frequence[w] = (frequence[w] || 0) + 1);
+
+    const topFiveWords = Object.entries(frequence)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 5)
+        .map(([word, count]) => ({ word, count}));
+
+    return { wordCounter, topFiveWords: topFiveWords }
 }
+
+module.exports = getPromptAmount;
